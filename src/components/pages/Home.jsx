18963f6e5a8f2bb0ea1677e2299e56e0b99d1c19
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-09-18 11:09:23 +0200

    Created pages: Checkout, CheckoutSuccess, Home and Product

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import useAPI from '../../hooks/API/useAPI'
+import { Link } from 'react-router-dom';
+
+
+function HomeApp() {
+    
+    const { data, isLoading, isError } = useAPI(
+        'https://api.noroff.dev/api/v1/online-shop',
+      );
+
+    return (
+        <div>
+            {data.map((data) => (
+                <div key={data.id}>
+                    <h2>{data.title}</h2>
+                    <p>{data.description}</p>
+                    <Link to={`/product/${data.id}`}>View Product!</Link>
+                    <img src={data.imageUrl}/>
+                </div>
+            ))}
+        </div>
+    )
+
+}
+
+export default HomeApp;
--- src/components/pages/Product.jsx
+++ src/components/pages/Product.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useAPI from "../../hooks/API/useAPI";
 
 function Product() {
-    return <div>Products</div>
+    let { id } = useParams();
+
+    const { data, isLoading, isError } = useAPI(
+        `https://api.noroff.dev/api/v1/online-shop/${id}`,
+      );
+
+    
+
+    return (
+        <div className="individual-product">Individual post id: {data.id}
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+            <img src={data.imageUrl}/>
+            <div className="product-price">
+                <p>{data.price}</p>
+                <p>{data.discountedPrice}</p>
+            </div>
+            <div>
+                <h2>Reviews</h2>
+                {data.reviews && data.reviews.length > 0 ? (
+                <ul>
+                {data.reviews.map((review) => (
+                    <li key={review.id}>
+                        <strong>{review.username}</strong>
+                        <p>Rating: {review.rating}</p>
+                        <p>{review.description}</p>
+                        <hr></hr>
+                    </li>
+                ))}
+                
+                </ul>
+                ) : (
+                    <p>No reviews available</p>
+                )}
+
+            </div>
+        </div>
+        )
 }
 
 export default Product;
