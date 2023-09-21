cdf5bd61a9f223da468448b6379554417cbeba29
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-09-21 09:20:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,23 +5,23 @@ import { Routes, Route } from 'react-router-dom'
 import Product from './components/pages/Product'
 import Cart from './components/pages/Cart'
 import Checkout from './components/pages/Checkout';
+import HomeApp from './components/pages/Home';
+
 
 function App() {
-
   return (
     <div>
-        <Layout>
-            <CartIcon />
-        </Layout>
-        <Routes>
-            <Route path="checkout" element={<Checkout />} />
-            <Route path="product" element={<Product />} />
-            <Route path="cart" element={<Cart />} />
-        </Routes>
-    </div>
+    <Layout>
+        <CartIcon />
+    </Layout>
+    <Routes>
+        <Route path="home" element={<HomeApp />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="product/:id" element={<Product />} />
+        <Route path="cart" element={<Cart />} />
+    </Routes>
+</div>
   );
 }
-
-
 
 export default App;
