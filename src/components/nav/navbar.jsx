3f54b3111e394575a65095c04134551fddcd26eb
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-09-25 14:11:38 +0200

    Improving the Navbar and Installing react-router-bootstrap

--- src/components/nav/navbar.jsx
+++ src/components/nav/navbar.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 function MyNavbar({ title, links }) {
     return (
-        <nav>
-            <h1>{title}</h1>
-            <ul>
-                {links.map((link, index) => (
-                    <li key={index}>
-                        <Link to={link.url}>{link.label}</Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <LinkContainer to="/home">
+                    <Navbar.Brand href="home">MyLittleShop</Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle aria-controls="basic-navnar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                    {links.map((link, index) => (
+                   
+                   <Link key={index} to={link.url}>{link.label}</Link>
+
+           ))}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+ 
+        </Navbar>
     )
 }
 
