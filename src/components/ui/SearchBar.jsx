0d0e533f0bdc6a70627de05c70256c1737471c20
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-10-09 12:13:12 +0200

    Created a searchbar when Fetching API

--- src/components/ui/SearchBar.jsx
+++ src/components/ui/SearchBar.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import useAPI from "../../hooks/API/useAPI";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Row, Col } from "react-bootstrap";
+import styles from '../../scss/ProductCards/ProductCards.module.scss';
 
+import '../../App.scss';
 
 function SearchBar() {
-    var url = '';
+    
     const [userInput, setUserInput] = useState('');
     const { data: ApiData, isLoading, isError } = useAPI(`https://api.noroff.dev/api/v1/online-shop?query=${userInput}`);
 
@@ -16,9 +18,14 @@ function SearchBar() {
     const filteredData = ApiData.filter((item) => 
     item.title.toLowerCase().includes(userInput.toLowerCase())
     );
+    const bootstrapClassesCard = "flex-fill";
+    const cardClass = styles.cardington;
+
+    const combinedClasses = `${bootstrapClassesCard} ${cardClass}`
 
 
     return (
+        
         <div>
             <input className="searchbar" type='text' value={userInput} placeholder='Search...' onChange={handleInputChange}></input>
             <div>
@@ -28,16 +35,24 @@ function SearchBar() {
                     <p>Error fetching data.</p>
                 ) : (
                     <div>
-                        {filteredData.map((item) => (
-                            <Card style={{ width: '16rem' }} key={item.id} className="product-card flex">
-                                <Card.Img className="shop-img" src={item.imageUrl} variant="top" alt={item.title} />
-                                <Card.Body>   
-                                    <Card.Title className="text-align-center">{item.title}</Card.Title>
-                                    <Card.Text>{item.description}</Card.Text>
-                                    <Button href={`/product/${item.id}`}>View Product!</Button>
-                                </Card.Body>
-                            </Card>
-                        ))}
+                        <Row id="rowstyling">
+                            {filteredData.map((item) => (
+                                <Col className="d-flex" xs={12} sm={6} md={4} lg={3}>
+                                    <Card style={{ width: '16rem', margin: '2px' }} key={item.id} className={`w-100 ${combinedClasses}`}>   <div className={styles. cardImgContainer}>
+                                            <Card.Img className="shop-img" src={item.imageUrl} variant="top" alt={item.title} />
+                                        </div>
+                                        <div className="card-content-wrapper d-flex flex-column">
+                                            <Card.Body className={styles.cardBodyClass}>   
+                                                <Card.Title className={styles.cardTitleClass}>{item.title}</Card.Title>
+                                                <Card.Text className={styles.cardTextClass}>{item.description}</Card.Text>
+                                                
+                                            </Card.Body>
+                                            <Button className={styles.cardButtonClass} href={`/product/${item.id}`}>View Product!</Button>
+                                        </div>
+                                    </Card>
+                                </Col>
+                            ))}
+                         </Row>
                     </div>
                 )}
             </div>
