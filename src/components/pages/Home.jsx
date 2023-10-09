eb310fc3146328a7a29997bb880d2abea5c7351a
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-10-09 12:14:11 +0200

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,16 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import useAPI from '../../hooks/API/useAPI'
-import { Link } from 'react-router-dom';
+import React from 'react';
+
+
 import SearchBar from '../ui/SearchBar';
 
 function HomeApp() {
-    const [userInput, setUserInput] = useState('');
-    const { data, isLoading, isError } = useAPI("https://api.noroff.dev/api/v1/online-shop");
 
 
     return (
         <div>
-        
         <SearchBar />
         </div>
     )
