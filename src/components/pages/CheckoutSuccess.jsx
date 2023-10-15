6bb44ab5459ddd2526b046aa8f18a9807d381893
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-10-15 10:50:08 +0200

    Update CheckoutSuccess.jsx

--- src/components/pages/CheckoutSuccess.jsx
+++ src/components/pages/CheckoutSuccess.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Alert } from "react-bootstrap";
+import styles from "../../scss/cart/Alert.module.scss"
 
 function CheckoutSuccess() {
 
     return (
-    <div>Success</div>
+        <Alert className={styles.Alert} variant="success">
+            <Alert.Heading>Your order was successfully placed!</Alert.Heading>
+            <p>
+                Thank you for taking your time and looking through my project!
+            </p>
+            <hr />
+            <p className="mb-0">
+                <a href="/home">Click here</a> to go back home!
+            </p>
+        </Alert>
     )
 }
 
